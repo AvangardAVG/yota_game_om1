77ee4fd364471cfdbd243103756c811d14b8da50
kmosk <k.mashkovski11@gmail.com>
2019-07-22 15:02:04 +0600

    fix mobile interaction

--- script.js
+++ script.js
@@ -299,8 +299,57 @@ onmousemove = function(e) {
     mouse_x = e.pageX - cnvs.offsetLeft;
     mouse_y = e.pageY - cnvs.offsetTop;
 };
+ontouchmove = function(e) {
+    mouse_x = e.pageX - cnvs.offsetLeft;
+    mouse_y = e.pageY - cnvs.offsetTop;
+};
 
 cnvs.onmousedown = function(e) {
+    const mouse_down_x = e.pageX - cnvs.offsetLeft;
+    const mouse_down_y = e.pageY - cnvs.offsetTop;
+
+    var dx = tar.gb_counter.btn_x - mouse_down_x;
+    var dy = tar.gb_counter.btn_y - mouse_down_y;
+    var r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    if (r < btn_rad) {
+        if (tar.gb_counter.infinity) tar.gb_counter.infinity = false;
+        if (tar.gb_counter.number - gbs[gb_btn_counter] < 0) {
+            tar.gb_counter.number = 0;
+        } else {
+            tar.gb_counter.number -= gbs[gb_btn_counter];
+        }
+        gb_btn_timer = btn_timer;
+        if (gb_btn_counter !== 3) {
+            gb_btn_counter++;
+        }
+    }
+
+    dx = tar.min_counter.btn_x - mouse_down_x;
+    dy = tar.min_counter.btn_y - mouse_down_y;
+    r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
+    if (r < btn_rad) {
+        if (tar.min_counter.number - mins[min_btn_counter] < 0) {
+            tar.min_counter.number = 0;
+        } else {
+            tar.min_counter.number -= mins[min_btn_counter];
+        }
+        min_btn_timer = btn_timer;
+        if (min_btn_counter !== 3) {
+            min_btn_counter++;
+        }
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        if (Math.abs(mouse_down_x - arr[i].x) <= arr[i].width / 2 &&
+            Math.abs(mouse_down_y - arr[i].y) <= arr[i].height / 2) {
+            arr[i].caught = true;
+            caught = arr[i];
+            break;
+        }
+    }
+};
+
+cnvs.ontouchstart = function(e) {
     const mouse_down_x = e.pageX - cnvs.offsetLeft;
     const mouse_down_y = e.pageY - cnvs.offsetTop;
 
@@ -351,6 +400,33 @@ cnvs.onmouseleave = function() {
 };
 
 cnvs.onmouseup = function(e) {
+    if (caught != null) {
+        const mouse_up_x = e.pageX - cnvs.offsetLeft;
+        const mouse_up_y = e.pageY - cnvs.offsetTop;
+        if (mouse_up_x - tar.x > -tar.width / 2 && mouse_up_x - tar.x < tar.width / 2 &&
+            mouse_up_y - tar.y > -tar.height / 2 && mouse_up_y - tar.y < tar.height / 2) {
+            if (caught.type === gb_type) {
+                if (tar.gb_counter.number + caught.number > 50) {
+                    tar.gb_counter.number = 51;
+                    tar.gb_counter.infinity = true;
+                } else {
+                    tar.gb_counter.number += caught.number;
+                }
+            } else {
+                if (tar.min_counter.number + caught.number > 2000) {
+                    tar.min_counter.number = 2000;
+                } else {
+                    tar.min_counter.number += caught.number;
+                }
+            }
+            caught.new_position();
+        }
+        caught.caught = false;
+        caught = null;
+    }
+};
+
+cnvs.ontouchend = function (e) {
     if (caught != null) {
         const mouse_up_x = e.pageX - cnvs.offsetLeft;
         const mouse_up_y = e.pageY - cnvs.offsetTop;
